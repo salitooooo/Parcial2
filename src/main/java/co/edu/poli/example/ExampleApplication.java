b22ac24d9b190d818f4edb63034d929a80c4ee95
salitooooo <salodorado2004@gmail.com>
2025-10-16 16:37:02 -0500

    parcial terminado

--- src/main/java/co/edu/poli/example/ExampleApplication.java
+++ src/main/java/co/edu/poli/example/ExampleApplication.java
@@ -10,6 +10,8 @@ public class ExampleApplication {
 
 	public static void main(String[] args) {	
 		Dotenv dotenv = Dotenv.configure().load();
+		System.out.println(dotenv.get("DB_USER_SUPABASE"));
+		System.out.println(dotenv.get("DB_PWD_SUPABASE"));
         System.setProperty("DB_USER_SUPABASE", dotenv.get("DB_USER_SUPABASE"));
         System.setProperty("DB_PWD_SUPABASE", dotenv.get("DB_PWD_SUPABASE"));	
 		SpringApplication.run(ExampleApplication.class, args);
